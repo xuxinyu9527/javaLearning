bacd392ce1c6f105647a67c73a9efd72001133f5
xuxinyu9527 <xuxinyu9527@gmail.com>
2020-06-27 20:40:26 +0800

    first commit

---
+++ src/test/java/com/java_learning/javaLearning/JavaLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_learning.javaLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
